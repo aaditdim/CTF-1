737c85cd43da06cc887c396697f348603e427a5d
valageri <valageri@usc.edu>
2022-10-12 17:45:31 -0700

    ssh test

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
 	session_start();
 	// $db = mysqli_connect('localhost', 'bank3db', 'BakeshopSequelHenceExpansionAptlyEverglade', 'users');
 
+
 	function generateRandomString($length = 50) {
 	    return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
 	}
