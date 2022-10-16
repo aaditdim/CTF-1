9e484a1674e0c59f582902e72c91f09d12c57ae5
valageri <valageri@usc.edu>
2022-10-16 15:30:17 -0700

    Updated token insertion command

--- login.php
+++ login.php
@@ -1,9 +1,6 @@
 <?php
 	// Database connection
 	include('config/db.php');
-	session_start();
-	// $db = mysqli_connect('localhost', 'bank3db', 'BakeshopSequelHenceExpansionAptlyEverglade', 'users');
-
 
 	function generateRandomString($length = 50) {
 	    return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
@@ -15,31 +12,32 @@
 	if(empty($username) || empty($password)) {
 		print("Please provide username and password");
 	} else {
-		print("cookie");
-		print_r($_COOKIE);
-		print("cookie print end here\n");
-		
-		#$stmt = $db->prepare("SELECT * FROM bank WHERE user_id=? LIMIT 1");
-		#$stmt->bind_param("s", $username);
-		#$stmt->execute();
-		#$result = $stmt->get_result();
+		mysqli_report(MYSQLI_REPORT_ERROR);
+		$stmt = $db->prepare("SELECT * FROM bank WHERE user_id=? LIMIT 1");
+		$stmt->bind_param("s", $username);
+		$stmt->execute();
+		$result = $stmt->get_result();
 
-		$user_check_query = "SELECT * FROM bank WHERE user_id='$username' LIMIT 1";
-		$result = mysqli_query($db, $user_check_query);
-		$user = mysqli_fetch_assoc($result);
-	
-		if(count($user) > 0) { // valid user exist
-		#if($result->num_rows > 0) {
-			#$user = $result->fetch_assoc();
+		if($result->num_rows > 0) {
+			$user = $result->fetch_assoc();
 			if(password_verify($password, $user["password"])) {
 
 		                $token = generateRandomString();
 		                $expiry = time() + 300;
-		                $insert_token = "INSERT INTO `session` (`user_id`, `token`, `expiry_time`) VALUES('$username', '$token', '$expiry')";
-		                mysqli_query($db, $insert_token);
+
+				$stmt = $db->prepare("REPLACE INTO session (user_id, token, expiry_time) VALUES(?, ?, ?)");
+				$stmt->bind_param("ssi", $username, $token, $expiry);
+				$stmt->execute();
+
+				#$insert_token = "REPLACE INTO `session` (`user_id`, `token`, `expiry_time`) VALUES('$username', '$token', '$expiry')";
+		                #if(mysqli_query($db, $insert_token)) {
+				#	error_log("New record created successfully");
+				#} else {
+				#	error_log("Error: " . $sql . "<br>" . mysqli_error($db));
+				#}
+
 		                setcookie("user_id", $username);
 		                setcookie("token", $token);
-	
 				error_log("Valid password", 0);
 				print('You are now logged in <br>');
 			} else {
